84ea8b9cebcc43f5b6dba22d502178da9631af06
Clifton Palmer <clifton.james.palmer@protonmail.com>
2021-09-13 13:17:33 -0500

    Better interface with db

--- socket/db.js
+++ socket/db.js
@@ -2,8 +2,8 @@ const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
     host: 'db', 
-    user: 'root', 
-    password: 'admin',
+    user: 'socket',
+    password: 'socketpw',
     connectionLimit: 5
 });
 
@@ -13,10 +13,16 @@ async function testdb() {
         conn = await pool.getConnection();
         const rows = await conn.query("SELECT 1 as val");
         console.log(rows); //[ {val: 1}, meta: ... ]
-        const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+
+        var res = await conn.query(
+            "CREATE TABLE IF NOT EXISTS go.myTable ( id INT, tag VARCHAR(255) )"
+        );
+        console.log(res);
+
+        res = await conn.query("INSERT INTO go.myTable value (?, ?)", [1, "mariadb"]);
         console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
     } catch (err) {
-        throw err;
+        console.log(err);
     } finally {
         if (conn) return conn.end();
     }
